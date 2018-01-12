ec5c08a728dfd12406ac54b39812fa4cf0bbb6b6
Sereno Mickaël <serenomickael@gmail.com>
2018-01-12 18:14:29 +0100

    Treat the variable GET as an int

--- web/AJAX/fetchTasks.php
+++ web/AJAX/fetchTasks.php
@@ -2,5 +2,10 @@
 	session_start();
 	require_once __DIR__.'/../../PSQL/TaskRqst.php';
 
+	//TODO check if we can access to this project
 
+	$taskRqst = new TaskRqst();
+	$result = $taskRqst->getTasks((int)($_GET["projectID"]));
+
+	echo(json_encode($result));
 ?>
